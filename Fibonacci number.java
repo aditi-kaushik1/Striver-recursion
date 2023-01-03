7359e8a4e467ae71a49a42323af9793e85006384
Aditi Kaushik <47269969+aditi-kaushik1@users.noreply.github.com>
2023-01-03 22:25:30 +0530

    Update Fibonacci number.java

--- Fibonacci number.java
+++ Fibonacci number.java
@@ -4,9 +4,9 @@ class HelloWorld {
     }
     
     public static int fibonacci(int n) {
-        if(n == 1)
+        if(n == 0)
             return 0;
-        if(n == 2)
+        if(n == 1)
             return 1;
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
